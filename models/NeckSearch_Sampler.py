50ccb1cc7c090713e9b7949ea1bdd38632ea9e35
z1j1n1 <905005832@qq.com>
2022-03-12 20:41:41 +0800

    修改Sampler使其能够选择P3-P7的输出，随机生成一个arch

--- models/NeckSearch_Sampler.py
+++ models/NeckSearch_Sampler.py
@@ -7,8 +7,8 @@ with open("yolov5s.yaml", encoding='ascii', errors='ignore') as f:
 
 ConvChannels = [-1,128,256,512]
 C3Channels = [128,256,512]
-Strides = {'P3':8, 'P4':16, 'P5':32} #目前只在P3,P4,P5,确定Validation时为什么出现image_size=672可以考虑P6,P7
-NConcat = 6
+Strides = {'P3':8, 'P4':16, 'P5':32, 'P6':64, 'P7':128} #目前只在P3,P4,P5,确定Validation时为什么出现image_size=672可以考虑P6,P7
+NConcat = 8
 
 feature = [[3,'P3'],[5,'P4'],[9,'P5']]
 Output = {'P3':3, 'P4':5, 'P5':9}
@@ -26,12 +26,14 @@ for i in range(NConcat):
     if OutStride[0] in ['P3','P4','P5']:
         Output[OutStride[0]] = LayerCount
     Neck.append([[x1[0],x2[0]],1,'ConcatCell',[C3Channel,OutStride[1]/Strides[x2[1]],ConvChannel1,ConvChannel2]])
+    print([x1[0],x2[0]],1,'ConcatCell',[C3Channel,OutStride[1]/Strides[x2[1]],ConvChannel1,ConvChannel2],OutStride[0])
     LayerCount += 1
 
 Neck.append([list(Output.values()),1,'Detect',['nc', 'anchors']])
+print([list(Output.values()),1,'Detect',['nc', 'anchors']])
 arch['head'] = Neck
 
-with open('yolov5s-NeckSearch-random.yaml', "w") as f:
+with open('yolov5s-NeckSearch-random-P7.yaml', "w") as f:
     yaml.default_flow_style = False
     yaml.dump(arch,f)
 
