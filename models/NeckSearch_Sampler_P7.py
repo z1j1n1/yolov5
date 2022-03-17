83f059cf184e6ce1867fe6f9ade26c1f34c8ef3c
z1j1n1 <905005832@qq.com>
2022-03-17 10:22:02 +0800

    增加了抽取多个Architectures

--- models/NeckSearch_Sampler_P7.py
+++ models/NeckSearch_Sampler_P7.py
@@ -11,31 +11,34 @@ Strides = {'P3':8, 'P4':16, 'P5':32, 'P6':64, 'P7':128}
 C3Types = ['C3','BottleneckCSP','BottleneckCSP2','C3TR','C3SPP','C3Ghost']
 NConcat = 8
 
-feature = [[3,'P3'],[5,'P4'],[9,'P5']]
-Output = {'P3':3, 'P4':5, 'P5':9}
-LayerCount = 10
+SampledArchs = 5
 
-Neck = []
-for i in range(NConcat):
-    x1,x2 = random.sample(feature,2)
-    ConvChannel1 = random.sample(ConvChannels,1)[0]
-    ConvChannel2 = random.sample(ConvChannels,1)[0]
-    C3Channel = random.sample(C3Channels,1)[0]
-    OutStride = random.sample(Strides.items(),1)[0]
-    C3Type = random.sample(C3Types,1)[0]
+for ArchID in range(SampledArchs):
+    feature = [[3, 'P3'], [5, 'P4'], [9, 'P5']]
+    Output = {'P3': 3, 'P4': 5, 'P5': 9}
+    LayerCount = 10
+    Neck = []
 
-    feature.append([LayerCount,OutStride[0]])
-    if OutStride[0] in ['P3','P4','P5']:
-        Output[OutStride[0]] = LayerCount
-    Neck.append([[x1[0],x2[0]],1,'ConcatCell',[C3Channel,OutStride[1]/Strides[x2[1]],ConvChannel1,ConvChannel2,C3Type]])
-    LayerCount += 1
+    for i in range(NConcat):
+        x1,x2 = random.sample(feature,2)
+        ConvChannel1 = random.sample(ConvChannels,1)[0]
+        ConvChannel2 = random.sample(ConvChannels,1)[0]
+        C3Channel = random.sample(C3Channels,1)[0]
+        OutStride = random.sample(Strides.items(),1)[0]
+        C3Type = random.sample(C3Types,1)[0]
 
-Neck.append([list(Output.values()),1,'Detect',['nc', 'anchors']])
-arch['head'] = Neck
+        feature.append([LayerCount,OutStride[0]])
+        if OutStride[0] in ['P3','P4','P5']:
+            Output[OutStride[0]] = LayerCount
+        Neck.append([[x1[0],x2[0]],1,'ConcatCell',[C3Channel,OutStride[1]/Strides[x2[1]],ConvChannel1,ConvChannel2,C3Type]])
+        LayerCount += 1
 
-with open('yolov5s-NeckSearch-random-P7.yaml', "w") as f:
-    yaml.default_flow_style = False
-    yaml.dump(arch,f)
+    Neck.append([list(Output.values()),1,'Detect',['nc', 'anchors']])
+    arch['head'] = Neck
+
+    with open('yolov5s-NeckSearch-random-P7-{}.yaml'.format(ArchID), "w") as f:
+        yaml.default_flow_style = False
+        yaml.dump(arch,f)
 
 
 
